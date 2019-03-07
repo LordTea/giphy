dc8f103269c3b28ee0e2f2a7f4eac64770897f5c
Thienan Nguyen <thienan.nguyen@ousd.us>
2019-03-07 01:56:06 +0000

    Set up API and search system, working on the user search.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,28 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var API = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+
 $("#search-button").click(function(){
-  
-  
-  
+    // Get Value
+    var search = $("#search-term").val();
+    var giflist = [];
+    
+    // GET From API
+    $.ajax({
+        url: API,
+        method: "GET",
+        success: function(response){
+            for (var i = 0; i < response["data"].length; i++){
+                //if ((response.data[i].slug).includes("dog")){
+                    giflist.push(response.data[0].images.original.url);
+                //}
+            }
+        }
+    });
+    
+    // Display
+    $(".text-center").empty();
+    $(".text-center").append("<img src='"+giflist[0]+"'>");
 });
 
